bb05fca8bc1e19555be8a1da9b609dbf3397e817
Matt <mtaylor@tradestation.com>
2011-06-29 16:54:54 -0500

    Started on the board sleection screen

--- src/Chs.HtmlKanban/Chs.HtmlKanban.Web/Controllers/BoardController.cs
+++ src/Chs.HtmlKanban/Chs.HtmlKanban.Web/Controllers/BoardController.cs
@@ -4,11 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Raven.Client;
+using CastleHillsSoftware.HtmlKanban.Web.Models;
 
 namespace CastleHillsSoftware.HtmlKanban.Web.Controllers
 {
-    public class BoardController : RavenController
-    {
-        public BoardController(IDocumentStore store) : base(store) { }
-    }
+	public class BoardController : RavenController
+	{
+		public BoardController(IDocumentStore store) : base(store) { }
+
+		public ActionResult List()
+		{
+			var boards = this.ExecuteQuery<IEnumerable<StickyBoard>>(session => from board in session.Query<StickyBoard>()
+					   orderby board.Title
+					   select board);
+			return new JsonResult() { Data = boards, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+		}
+
+		public ActionResult Create()
+		{
+			StickyBoard board = new StickyBoard() { Title = "New Sticky Board" };
+			ExecuteNonQuery(session =>
+			{
+				session.Store(board);
+				session.SaveChanges();
+			});
+			return new JsonResult() { Data = board, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+		}
+	}
 }
