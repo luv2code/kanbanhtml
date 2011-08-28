fe39f21faeed0e7dfd90b776c4fad2191da74611
Matthew Taylor <luv2code@gmail.com>
2011-08-28 17:29:20 -0500

    fixed a knockout related issue.

--- src/Chs.HtmlKanban/Chs.HtmlKanban.Web/client/viewmodels/boardSelectionViewModel.js
+++ src/Chs.HtmlKanban/Chs.HtmlKanban.Web/client/viewmodels/boardSelectionViewModel.js
@@ -1,4 +1,6 @@
-﻿define(['presenter', 'services/kanban'], function (presenter, kanbanService) {
+﻿/// <reference path="../lib/knockout.js" />
+
+define(['presenter', 'services/kanban'], function (presenter, kanbanService) {
     var viewModel = function () {
         kanbanService.listBoards(0, 0, '', function (data) {
             this.boardList = data;
@@ -7,15 +9,16 @@
     viewModel.prototype = {
         boards: [],
         createBoard: function () {
-            if (this.newBoardName.length < 1) {
+            alert(this.newBoardName());
+            if (this.newBoardName().length < 1) {
                 alert("Board Name must not be empty.");
             } else {
-                
+
             }
         },
         refreshList: function () {
         },
-        newBoardName: ''
+        newBoardName: 'sdfg'
     };
 
     return viewModel;
